75eb479b93ea78b5a812e4d06dc0201b9c8e9676
mrhoseah <hoseahkplgt@gmail.com>
2022-05-11 03:34:00 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './index.scss'
+import NavBar from './components/NavBar'
+
+function App() {
+
+  return (
+
+    <div className="md:container md:mx-auto">
+      <header>
+        <NavBar />
+      </header>
+    </div>
+  )
+}
+
+export default App
